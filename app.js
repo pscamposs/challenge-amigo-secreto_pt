88792c4c000a9efbbd0b2576c322be589861beb9
pscamposs <pscampos221@gmail.com>
2025-03-12 10:28:03 -0300

    Adicionar lógica para adicionar e sortear amigos

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+const inputNome = document.getElementById("amigo");
+const containerLista = document.querySelector(".name-list");
+const containerResultado = document.querySelector(".result-list");
+
+const nomes = [];
+
+function adicionarAmigo() {
+  const nome = inputNome.value.trim();
+  if (nome === "") {
+    alert("Você deve inserir um nome válido.");
+    return;
+  }
+  nomes.push(nome);
+  containerLista.innerHTML += `<li>${nome}</li>`;
+}
+
+function sortearAmigo() {
+  if (nomes.length == 0) {
+    alert("Você deve adicionar amigos para sortear.");
+    return;
+  }
+  const random = Math.floor(Math.random() * nomes.length);
+  const nomeSorteado = nomes[random];
+  containerResultado.innerHTML = `<li>${nomeSorteado}</li>`;
+}
